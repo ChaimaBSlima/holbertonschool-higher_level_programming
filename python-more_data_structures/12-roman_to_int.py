26985c25d9f87204fe11a04ec734f5e85414fb7c
ChaimaBSlima <chaimabslima@outlook.fr>
2024-01-31 10:33:47 +0100

    task 12 first try

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or not roman_string:
+        return (0)
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                  'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    a = 0
+    for i in reversed(roman_string):
+        b = roman[i]
+        if b < a:
+            result -= b
+        else:
+            result += b
+        a =b
+    return (result)
