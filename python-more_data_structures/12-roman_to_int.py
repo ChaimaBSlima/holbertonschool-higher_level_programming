0f5cfb9701ab6d84689b2a934f33eb920c284aae
ChaimaBSlima <chaimabslima@outlook.fr>
2024-01-31 10:52:27 +0100

    changing identients

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if not isinstance(roman_string, str):
         return (0)
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     a = 0
     for i in roman_string[::-1]:
