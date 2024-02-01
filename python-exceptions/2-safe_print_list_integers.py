787badf742430bd44638b706611df874f74b2062
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-01 09:45:40 +0100

    Replace while by for and use one except

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     i = 0
-    while (i < x):
+    for j in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
-        except IndexError:
-            raise
-        except Exception as e:
-            pass
-        else:
+            print("{:d}".format(my_list[j]), end='')
             i += 1
-        i += 1
+        except (TypeError, ValueError):
+            pass
     print()
     return (i)
