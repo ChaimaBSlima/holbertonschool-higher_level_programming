413060b77c11efd5f582e7517e35362b74cba1d5
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-01 09:38:43 +0100

    task 0 remplacing for by while

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    i = 0
     try:
-        for i in range(0, x):
-            print("{}".format(my_list[i]), end="")
+        while (i < x):
+            print("{:d}".format(my_list[i]), end="")
+            i += 1
     except Exception as e:
         pass
     print()
