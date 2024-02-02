862f4915f72dfa86cc6d2b4cd9a9fccc9a6ad06f
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-02 09:29:28 +0100

    task 1 add documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+    task 1 class square
+"""
+
+
 class Square:
+    """ Defining a Square size """
     def __init__(self, size):
-        """ Defining a Square """
+        """ self arg have the size """
         self.__size = size
