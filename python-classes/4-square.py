79cdda195051edd9460226d03dc63c51913d089c
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-03 20:23:47 +0100

    task 4 documentation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,30 +5,27 @@
 
 
 class Square:
-    """ 
+    """
     Define a class Square.
     """
 
-
     def __init__(self, size=0):
-        self.size = size
-
+        self._Square__size = size
 
     @property
     def size(self):
-        return (self.__size)
-
+        return (self._Square__size)
 
     @size.setter
     def size(self, value):
-        """ method to initalize value"""
-        if type(value) != int:
-            raise TypeError('size must be an integer')
-        if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
-
+        """
+        size.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
 
     def area(self):
-        """ method to return current square area"""
-        return (self.__size**2)
+        return (self._Square__size**2)
