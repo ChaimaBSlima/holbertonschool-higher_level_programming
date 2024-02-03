72c1c82bccac8bbf848717f38d0645c9f6c845a7
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-03 20:28:05 +0100

    task 5 documentaion

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -8,28 +8,27 @@ class Square:
     """ Define a class Square."""
 
     def __init__(self, size=0):
-        """Method to init"""
         self.size = size
 
     @property
     def size(self):
-        """method retreive size"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """
+        size
+        """
+        if (type(value) != int):
             raise TypeError('size must be an integer')
-        if value < 0:
+        if (value < 0):
             raise ValueError('size must be >= 0')
         self.__size = value
 
     def area(self):
-        """return square area"""
         return (self.__size**2)
 
     def my_print(self):
-        """print square with char #"""
         if self.__size is 0:
             print()
 
