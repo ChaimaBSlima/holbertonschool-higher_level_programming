8658d65ad109237334cbc9742dfebd6d8ac443b9
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-08 19:01:02 +0100

    test 5 and 6 and 7

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""task 5: 5. Geometry module"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+    pass
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""task 6: 6.Improve Geometry"""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        raise Exception("area() is not implemented")
