01e60b3721963590c2726b251a059967f81d2e3d
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-13 09:14:10 +0100

    task 16 saves jsonified object to file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -27,3 +27,11 @@ class Base:
             return ("[]")
         else:
             return (dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        "Task 16: saves jsonified object to file"
+        if list_objs is not None:
+            list_objs = [i.to_dictionary() for i in list_objs]
+        with open("[].json".format(cls.__name__), "w", encoding="utf-8") as f:
+            f.write(cls.to_json_string(list_objs))
