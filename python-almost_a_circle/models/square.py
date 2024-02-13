bcc1017f60f9543e4537a67aeccaf46b974bcd61
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-13 08:42:32 +0100

    task 10 the square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" python-almost_a_circle project: Module for square
+    starting at task 10
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+
+        Args:
+            size (int): size of the side of the square.
+            x (int): x
+            y(int): y
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        "Task 10:  return string informations"
+        return ('[{}] ({}) {}/{} - {}'.format(
+            type(self).__name__,
+            self.id, self.x,
+            self.y, self.width,))
