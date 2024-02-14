1d0478e74d48dedf6c9b6c37177dba0aaaf7820b
ChaimaBSlima <chaimabslima@outlook.fr>
2024-02-14 12:42:07 +0100

    task 19 Load string from a file and unjsonifies

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -59,3 +59,13 @@ class Base:
             chaima = None
         chaima.update(**dictionary)
         return chaima
+
+    @classmethod
+    def load_from_file(cls):
+        """ Task 19: Load string from a file and unjsonifies"""
+        from os import path
+        file = "[].json".format(cls.__name__)
+        if not path.isfile(file):
+            return ([])
+        with open(file, "r", encoding="utf-8") as f:
+            return ([cls.create(**i) for i in cls.from_json_string(f.read())])
