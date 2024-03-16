24ce93893ec376067e000019727570a7ad173487
ChaimaBSlima <chaimabslima@outlook.fr>
2024-03-16 22:10:37 +0100

    tasks 12 13 14 15 16

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- A comment 
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- A comment 
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
