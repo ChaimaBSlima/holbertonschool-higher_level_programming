eead5ef4f1bc679230536767b6466a95af13fd82
ChaimaBSlima <chaimabslima@outlook.fr>
2024-03-23 19:27:06 +0100

    tasks 0 1

---
+++ python-object_relational_mapping/0-select_states.sql
@@ -0,0 +1,9 @@
+-- A comment 
+CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
+USE hbtn_0e_0_usa;
+CREATE TABLE IF NOT EXISTS states ( 
+	    id INT NOT NULL AUTO_INCREMENT, 
+	    name VARCHAR(256) NOT NULL,
+	    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("Tunisia"), ("Sfax"), ("Janduba"), ("Mednin"), ("sousse");
